28b0414a02e5aa5cd388b03256c212b53f69b375
Rubens Pessoa <pessoadebarros@gmail.com>
2018-02-10 20:57:55 -0300

    Ended first component

--- index.android.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { AppRegistry, Text } from 'react-native';
-
-const App = () => <Text>Some Text</Text>;
-
-AppRegistry.registerComponent('AwesomeProject', () => App);
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
-const App = () => <Header />;
+const App = () => <Header headerText="Albums" />;
 
 AppRegistry.registerComponent('AwesomeProject', () => App);
