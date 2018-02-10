d2125e7147f3eac876444305bcb7f0f3b7125e9e
Rubens Pessoa <pessoadebarros@gmail.com>
2018-02-10 16:42:59 -0300

    init repoo

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import ReactNative from 'react-native';
+import { AppRegistry, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+AppRegistry.registerComponent('AwesomeProject', () => App);
