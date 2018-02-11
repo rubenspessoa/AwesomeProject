c89b24df37a667c249c3bdbde78e3324e98d50d2
Rubens Pessoa <pessoadebarros@gmail.com>
2018-02-11 16:13:00 -0300

    Starting to use class components

---
+++ src/components/AlbumList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  componentWillMount() {
+    console.log('Component will mount');
+    debugger;
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List !!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
